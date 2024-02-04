a4ce40d2c27a38b8db00b3c1787e474ec281d21a
Adarsh01208 <adarshsingh01209@gmail.com>
2024-02-05 00:23:22 +0530

    Update restaurant data fetching and setting

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,8 +3,9 @@ export const LOGO_URL = "https://th.bing.com/th/id/OIP.fI94-Tb2Jba0CKogcVvhOwHaH
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
 //export const SWIGGY_URL = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D26.87560%26lng%3D80.91150%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING"
-
  export const SWIGGY_URL = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D26.7786419%26lng%3D80.8709872%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING"
+
+ //export const SWIGGY_URL = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D26.7716098%26lng%3D80.8751372%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING"
 
  //https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7716098&lng=80.8751372&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
 
