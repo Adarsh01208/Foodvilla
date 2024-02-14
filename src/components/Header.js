4cc130598e3d8a26e97cccdb04d8028cc74d4c85
Adarsh01208 <adarshsingh01209@gmail.com>
2024-02-14 15:26:30 +0530

    Refactor Header component layout and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,20 +10,18 @@ const Header = () => {
     const { loggedInUser } = useContext(UserContext)
     const cartItems = useSelector((store) => store.cart.items)
     return (
-        <div className="container-fliud d-flex justify-content-between border shadow  " >
-            <div className="img-fluid mx-5 p-2  ">
-                <img height={70} src={LOGO_URL} alt="logo_icon" />
+        <div className="container-fluid d-flex justify-content-between border shadow" >
+            <div className="img-fluid mx-5 p-2">
+                <img className="img-responsive" height={70} src={LOGO_URL} alt="logo_icon" />
             </div>
-            <div className="nav-items d-flex align-items-center ">
-                <ul className="d-flex align-items-center  mx-5 ">
-                    <li className="px-2 list-group-item mx-3 fw-semibold "> OnlineStatus: {onlineStatus ? "✅" : "🔴"} </li>
-                    <li className="px-2 list-group-item mx-3 fw-semibold ">  <Link className=" text-decoration-none text-dark" to="/grocery">Grocery</Link> </li>
-                    <li className="px-2 list-group-item mx-3 fw-semibold ">  <Link className=" text-decoration-none text-dark" to="/about">AboutUs</Link> </li>
+            <div className="nav-items d-flex align-items-center flex-column flex-md-row">
+                <ul className="d-flex align-items-center flex-wrap justify-content-center mx-5">
+                    <li className="px-2 list-group-item mx-3 fw-semibold"> OnlineStatus: {onlineStatus ? "✅" : "🔴"} </li>
+                    <li className="px-2 list-group-item mx-3 fw-semibold">  <Link className=" text-decoration-none text-dark" to="/about">AboutUs</Link> </li>
                     <li className="px-2 list-group-item mx-3 fw-semibold"><i class="fa-solid fa-group-arrows-rotate"></i> <Link className=" text-decoration-none text-dark" to="/">Home</Link> </li>
                     <li className="px-2  list-group-item mx-3 fw-semibold"> <i class="fa-regular fa-circle-question"></i> <Link className=" text-decoration-none text-dark" to="/contact">ContactUs</Link></li>
                     <li className="px-2 list-group-item mx-3 fw-semibold"> <i class="fa-solid fa-bag-shopping"></i> <Link className=" text-decoration-none text-dark" to="/cart">Cart ({cartItems.length}) items </Link> </li>
                     <li className="px-2 list-group-item mx-3 fw-semibold"> <i class="fa-regular fa-user"></i> <button className="btn btn-warning py-1"><Link className=" text-decoration-none" to="/login">Login</Link> </button> </li>
-                    <li className="px-2 list-group-item mx-3 fw-semibold"> {loggedInUser}  </li>
                 </ul>
             </div>
         </div>
