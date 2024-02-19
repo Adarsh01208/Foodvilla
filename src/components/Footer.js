8ead6e09cacb5b091fd37510761ca114e8c6587d
Adarsh01208 <adarshsingh01209@gmail.com>
2024-02-19 18:17:04 +0530

    Update package-lock.json and index.css, add Bootstrap and update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    const currYear = new Date().getFullYear();
-    return (
-        <footer className="bg-body-tertiary text-center mt-5 border  border-2">
-            <div className="text-center p-3 shadow-sm " >
-                Copyright &copy; {currYear} Made by <strong><a className="text-body" href="https://github.com/Adarsh01208"></a> @Adarsh</strong>
-            </div>
-        </footer>
-    )
+  const linkStyle = { textDecoration: 'none', color: 'inherit' };
+
+  return (
+    <footer className="bg-dark text-white py-5 mt-5">
+      <div className="container ">
+        <div className="row  text-center">
+          <div className="col-md-3">
+            <h5>Company</h5>
+            <ul className="list-unstyled">
+              <li><Link to="/about" style={linkStyle}>About us</Link></li>
+              <li><Link to="/team" style={linkStyle}>Team</Link></li>
+              <li><Link to="/careers" style={linkStyle}>Careers</Link></li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Contact</h5>
+            <ul className="list-unstyled">
+              <li><Link to="/help" style={linkStyle}>Help & Support</Link></li>
+              <li><Link to="/partner" style={linkStyle}>Partner with us</Link></li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Legal</h5>
+            <ul className="list-unstyled">
+              <li><Link to="/terms" style={linkStyle}>Terms & Conditions</Link></li>
+              <li><Link to="/privacy" style={linkStyle}>Privacy Policy</Link></li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Social</h5>
+            <ul className="list-unstyled">
+              <li><a href="https://www.facebook.com" style={linkStyle}>Facebook</a></li>
+              <li><a href="https://www.twitter.com" style={linkStyle}>Twitter</a></li>
+              <li><a href="https://www.instagram.com" style={linkStyle}>Instagram</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-center mt-4">
+          <p>&copy; {new Date().getFullYear()}<Link className='text-decoration-none' to="https://github.com/Adarsh01208" > @Adarsh01208</Link></p>
+        </div>
+      </div>
+    </footer>
+  );
 };
+
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,30 +6,55 @@ import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-    const onlineStatus = useOnlineStatus();
-    const { loggedInUser } = useContext(UserContext)
-    const cartItems = useSelector((store) => store.cart.items)
-    return (
-        <nav className="navbar navbar-expand-lg  bg-white" >
-            <div className="container-fluid d-flex justify-content-between shadow-sm" >
-                <div className="img-fluid mx-5 p-2 navbar-brand">
-                    <Link to="/">
-                        <img className="img-responsive" height={70} src={LOGO_URL} alt="logo_icon" />
-                    </Link>
-                </div>
-                <div >
-                    <ul className="d-flex align-items-center flex-wrap justify-content-center mx-5" style={{ cursor: "pointer" }} >
-                        <li className="px-2 list-group-item mx-3 fw-semibold">OnlineStatus: {onlineStatus ? "✅" : "🔴"}</li>
-                        <li className="px-2 list-group-item mx-3 fw-semibold"><Link to="/about"></Link> AboutUs </li>
-                        <li className="px-2 list-group-item mx-3 fw-semibold"><i class="fa-regular fa-circle-question"></i><Link to="/contact"></Link> ContactUs</li>
-                        <li className="px-2 list-group-item mx-3 fw-semibold"><i className="fas fa-shopping-cart" />
-                        <Link to="/cart">Cart({cartItems.length}) </Link> items</li>
-                        <li className="px-2 list-group-item mx-3 fw-semibold"><i className="fa-solid fa-user-tie" /><Link to="/login"></Link> Sign In</li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  const onlineStatus = useOnlineStatus();
+  const { loggedInUser } = useContext(UserContext)
+  const cartItems = useSelector((store) => store.cart.items)
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+      <div className="container">
+      <div className="img-fluid mx-5 p-2 navbar-brand">
+           <Link to="/">
+            <img className="img-responsive" height={70} src={LOGO_URL} alt="logo_icon" />
+           </Link>
+      </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item mb-2 nav-link">
+              OnlineStatus: {onlineStatus ? "✅" : "🔴"}
+            </li>
+            <li className="nav-item mb-2 nav-link ">
+              <Link className="link  " to="/about">AboutUs</Link>
+            </li>
+            <li className="nav-item mb-2 nav-link ">
+              <Link className="link" to="/contact"> <i class="fa-regular fa-circle-question" /> ContactUs</Link>
+            </li>
+           
+          </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+          <li className="nav-item mb-2 mx-1 nav-link ">
+            <Link className="link" to="/cart"> <i className="fas fa-shopping-cart" /> Cart({cartItems.length}) items </Link>
+          </li>
+          <li className="nav-item mb-2 mx-1 nav-link">
+            <Link className="link" to="/login"> <i className="fa-solid fa-user-tie" /> Sign In</Link>
+          </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+  );
 };
 
 export default Header;
